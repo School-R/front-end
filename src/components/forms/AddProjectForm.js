32da9a47436c0da52f102382b9ba0ce21e9dd38e
Catherine Kim <catherinesjkim@gmail.com>
2020-03-31 11:59:36 -0700

    Add Updated Files Because: - Added code to the AddProjectForm so that the form looks nicer and add functionality.
    This commit:
    - Uses styled-components for styling the form and used redux to bring the state from actions to the AddProjectForm component.

--- src/components/forms/AddProjectForm.js
+++ src/components/forms/AddProjectForm.js
@@ -1,21 +1,214 @@
-import React from 'react'
+// Catherine will add functionality and styles
+import React, { useState, useEffect } from 'react'
+import { connect, useDispatch } from 'react-redux'
+import { withRouter, useParams } from 'react-router-dom'
+import { postProject, saveEditProject } from '../../redux/actions'
+// import { AvForm, AvField } from 'availity-reactstrap-validation'
+// import { Button } from 'reactstrap'
+import style from "styled-components";
 
-const AddProjectForm = () => {
+const AddEditContainer = style.div`
+  display: flex;
+  height: 100%;
+`;
+
+const Form = style.div`
+	width: 80%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+const FormContainer = style.form`
+	width: 100%
+	padding: auto;
+	overflow: auto;
+	flex: 1;
+	margin-botton: 80px;
+`;
+
+const FormTitle = style.h1`
+	font-size: 30px;
+	font-weight: 900;
+  color: #363131;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-directions: column;
+`;
+
+const Label = style.label`
+	font-size: 15px;
+	font-weight: 600;
+	color: #8f8d8d;
+	margin-top: 10px;
+`;
+
+const Button = style.button`
+	background-color: #F99C1B;
+	color: #fff;
+	padding: 10px;
+	width: 100%;
+	border: none;
+	border-radius: 4px;
+	font-size: 14px;
+	font-weight: bold;
+	margin-top: 10px;
+	cursor: pointer;
+	&:disabled {
+      opacity: .5;
+    }
+    &:hover {
+      background-color: #FA7027;
+    }
+`;
+
+const FormInput = style.input`
+  border: 1px solid #a9a9a9;
+  padding: 10px;
+  width: 100%;
+  border-radius: 3px;
+  border: solid 1px #e2e0e0;
+  background-color: #f3f3f3;
+  box-sizing: border-box;
+  margin-top: 10px;
+`;
+
+// const FormRight = style.div`
+//   width: 66.66%;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   flex-direction: column;
+//   background-color: #2281bf;
+// `;
+
+const AddProjectForm = props => {
+	const dispatch = useDispatch();
+	const { id } = useParams();
+
+	const [project, setProject] = useState({
+    project_id: 'project_id',
+    title: '',
+    subject: '',
+    favorite: '',
+    description: '',
+    imgurl: ''
+	});
+
+	// useEffect(() => {
+	// 	dispatch(setProject())
+	// }, [dispatch])
+	
+	const handleChanges = event => {
+		setProject({ ...project, [event.target.name]: event.target.value })
+		console.log(event.target.name)
+	}
+
+	const handleNumbers = event => {
+		setProject({ ...project, [event.target.name]: parseInt(event.target.value) })
+		console.log(event.target.name)
+	}
+
+	const handleClick = event => {
+		setProject({ ...project, [ event.target.name]: (event.target.checked ? 1 : 0) })
+		console.log(event.target.name)
+	}
+
+	const submitForm = event => {
+		event.preventDefault();
+		props.postProject(localStorage.getItem("token"), project, props.history)
+	}
+
+	const editForm = event => {
+		event.preventDefault();
+		props.saveEditProperty(localStorage.getItem("token"), project, props.history)
+	}
+
 	return (
-		<form>
-			<input type='text' value='' placeholder='Project Name' />
-			<select type='dropdown' value='' placeholder='Choose a Subject'>
-				<option value=''>Computer Education</option>
-				<option value=''>Math</option>
-				<option value=''>Science</option>
-				<option value=''>History</option>
-				<option value=''>Languages</option>
-				<option value=''>Literature</option>
-			</select>
-			<textarea type='text' value='' placeholder='Instructions & Notes' />
-			<input type='url' id='image' placeholder='Insert an Image URL' />
-		</form>
-	)
+    <AddEditContainer>
+      <Form>
+        <FormTitle>
+          {props.editProjectStart ? "Edit" : "Add"} Project
+        </FormTitle>
+        <br />
+        <FormContainer
+          onSubmit={props.editProjectStart ? editForm : submitForm}>
+          {/* <Label htmlFor="title">Title</Label> */}
+          <FormInput
+            required
+            project_id="project_id"
+            type="text"
+            name="title"
+            value={project.title}
+            placeholder="Project Name"
+            onChange={handleChanges}
+          />
+          <br />
+          <Label htmlFor="subject">Subject</Label>
+          <br />
+          <select
+            required
+            type="dropdown"
+            name="subject"
+            value={project.subject}
+            placeholder="Choose a Subject"
+            onChange={handleChanges}>
+            <option value="">Computer Education</option>
+            <option value="">Math</option>
+            <option value="">Science</option>
+            <option value="">History</option>
+            <option value="">Languages</option>
+            <option value="">Literature</option>
+          </select>
+          <br />
+          <br />
+          <Label htmlFor="description">Description</Label>
+          <FormInput
+            required
+            type="text"
+            name="description"
+            value={project.description}
+            placeholder="Instructions & Notes"
+            onChange={handleChanges}
+          />
+          <FormInput
+            required
+            type="url"
+            name="imageurl"
+            id="imageurl"
+            value={project.imageurl}
+            placeholder="Insert an Image URL"
+            onChange={handleChanges}
+          />
+          {props.postProjectStart || props.saveEditProjectStart ? (
+            <Button type="submit" disabled>
+              Submitting...
+            </Button>
+          ) : (
+            <Button type="submit">Submit</Button>
+          )}
+          {(props.postProjectError || props.saveEditProjectError) && (
+            <p style={{ color: "red", textAlign: "center", marginTop: "10px" }}>
+              Well, looks like the Project was Not saved
+            </p>
+          )}
+        </FormContainer>
+      </Form>
+    </AddEditContainer>
+  );
 }
 
-export default AddProjectForm
+const mapStateToProps = state => ({
+  projects: state.projects,
+  postProjectError: state.postProjectError,
+  postProjectStart: state.postProjectStart,
+  editProjectStart: state.editProjectStart,
+  saveEditProjectStart: state.saveEditProjectStart,
+  saveEditProjectError: state.saveEditProjectError,
+  currentProject: state.currentProject,
+});
+
+export default connect(mapStateToProps, {postProject, saveEditProject })(
+	withRouter(AddProjectForm))
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,32 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { SET_ERROR,  GET_USERS } from './actions'
+import { 
+	SET_ERROR,  
+	GET_USERS,
+	GET_PROJECTS_START,
+	GET_PROJECTS_SUCCESS,
+	GET_PROJECTS_ERROR,
+	POST_PROJECT_START,
+	POST_PROJECT_SUCCESS,
+	POST_PROJECT_ERROR,
+	EDIT_PROJECT_START,
+	EDIT_PROJECT_ERROR,
+	SAVE_EDIT_PROJECT_START,
+	SAVE_EDIT_PROJECT_SUCCESS,
+	SAVE_EDIT_PROJECT_ERROR
+} from './actions'
 
 const initialState = {
-	users:[],
-	projects: [],
-	error: '',
-}
+  users: [],
+  projects: [],
+  error: "",
+  postProjectError: false,
+  postProjectStart: false,
+  editProjectStart: false,
+  saveEditProjectStart: false,
+	saveEditProjectError: false, 
+	currentProject: false
+};
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
